95b9230e9b9145e514dbfa1b898484ef781cf4ce
wearisome <tomita.ryosuke1986@gmail.com>
2026-01-28 15:32:58 +0900

    fix left main.

--- keyboards/yk/do52/config.h
+++ keyboards/yk/do52/config.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
-#define MASTER_RIGHT
+#define MASTER_LEFT
 
 #define PS2_PIO_USE_PIO1
 #define PS2_MOUSE_ROTATE 90
@@ -11,4 +11,4 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
 #undef TAPPING_TERM
-#define TAPPING_TERM 200
+#define TAPPING_TERM 100
