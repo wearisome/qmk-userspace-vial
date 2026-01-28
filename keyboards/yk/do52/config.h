a36b46f4f19ed04ccb65950a26f6c3b8ef7df1cc
wearisome <tomita.ryosuke1986@gmail.com>
2026-01-28 15:16:49 +0900

    Revert "Fixed"
    This reverts commit c59c421e3b8466bd96459249d5995df78d9d2f12.

--- keyboards/yk/do52/config.h
+++ keyboards/yk/do52/config.h
@@ -1,23 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
-#define EE_HANDS
+#define MASTER_RIGHT
 
-#ifdef PS2_DRIVER_INTERRUPT
+#define PS2_PIO_USE_PIO1
 #define PS2_MOUSE_ROTATE 90
 #define PS2_MOUSE_USE_REMOTE_MODE
-
-#define PS2_INT_INIT()  do {    \
-    EICRA |= ((1<<ISC21) |      \
-              (0<<ISC20));      \
-} while (0)
-#define PS2_INT_ON()  do {      \
-    EIMSK |= (1<<INT2);         \
-} while (0)
-#define PS2_INT_OFF() do {      \
-    EIMSK &= ~(1<<INT2);        \
-} while (0)
-#define PS2_INT_VECT   INT2_vect
-#endif
 // uncomment for left hand
 //#undef PS2_MOUSE_USE_REMOTE_MODE
 
--- keyboards/yk/do52/rules.mk
+++ keyboards/yk/do52/rules.mk
@@ -1,2 +1,2 @@
-#SERIAL_DRIVER = vendor
+SERIAL_DRIVER = vendor
 #CONSOLE_ENABLE = yes
